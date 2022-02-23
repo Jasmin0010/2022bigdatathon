1fde9a4828d139fbb06f46452f0899d0add0c4be
Jasmin0010 <sumichip4035@sookmyung.ac.kr>
2022-02-24 02:25:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-import re
-import numpy as np
-import collections
-from collections import Counter
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from bokeh.io import output_file, show
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, HoverTool
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import *
-from pandas import  *
+import csv
+from pandas import Series, DataFrame
+with open("MBTI_train.csv", 'r') as raw:
+    lines = raw.readlines()
+data_temp = csv.reader(lines)
+data = []
+for record in data_temp:
+    data.append(record)
+    
+df = DataFrame(data[1:])
+df.columns = ['TYPE','DESCRIPTION']
 
-import pandas as pd
-df = pd.read_csv("MBTI_train.csv")
-row = np.array((["types"],["description"]))
-arr = np.concatenate((df,[row]),axis= 0)
 df.head()
